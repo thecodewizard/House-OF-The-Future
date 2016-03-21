4113c860274aa2a7597107a7ce69e129ca638aca
Esteban Denis <esteban.denis.ed@gmail.com>
2016-03-21 10:42:55 +0100

    Interface Switched To Classes
    Using of classes to improve efficiency.

---
+++ House Of The Future.Common/DAL/AdamBoard1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace House_Of_The_Future.Common.DAL
+{
+    public class AdamBoard1
+    {
+        #region 172.23.49.101
+
+        #region general
+        public static void OpenConnetion1()
+        {
+            throw new NotImplementedException();
+        }
+        public static void CloseConnetion1()
+        {
+            throw new NotImplementedException();
+        }
+        public static bool CheckConnetion1()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region ventilator1
+        public static void TurnOnVentilator1()
+        {
+            throw new NotImplementedException();
+        }
+        public static void TurnOffVentilator1()
+        {
+            throw new NotImplementedException();
+        }
+        public static bool StatusVentilator1()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region lamp
+        public static void TurnOnLamp()
+        {
+            throw new NotImplementedException();
+        }
+        public static void TurnOffLamp()
+        {
+            throw new NotImplementedException();
+        }
+        public static bool StatusLamp()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region inputs
+        public static short StatusPotentiometer1()
+        {
+            throw new NotImplementedException();
+        }
+        public static short StatusPotentiometer2()
+        {
+            throw new NotImplementedException();
+        }
+        public static short StatusPotentiometer3()
+        {
+            throw new NotImplementedException();
+        }
+        public static bool StatusSwitch1()
+        {
+            throw new NotImplementedException();
+        }
+        public static bool StatusSwitch2()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #endregion
+    }
+}
